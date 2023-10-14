e0adf3b56fb22dfe562230ad70b2acfae96c2b7d
Kazim <syedkazinmkazmi689@gmail.com>
2023-10-14 10:13:24 -0700

    Jspractice

---
+++ Demo.js/Prac.js
@@ -0,0 +1,85 @@
+let x = 5;
+let myName = {
+    Name:"kazim",
+    age:23,
+    sex:"male"
+}
+//console.log(myName)
+myName.age = 55
+//console.log(myName)
+
+let mydetails = {
+    firstName:"Syed",
+    secondName:"Kazim",
+    age:"23",
+    id:[76,78,98,45],
+    height:"6.7",
+    eyecolor:"black",
+    haircolor:"black"
+
+}
+   // console.log(mydetails.id[1])
+
+
+    const car = ["toyotca","BMW", "Marcedes","Honda"]
+   // console.log(car)
+
+   //let mycar = ["toyota", "BMW", "mode", 2023, 2024,
+
+  
+   
+  // console.log(mycar)
+
+   const c =10;
+   const D =5;
+   const e =6;
+   const g = c*D+e
+ // console.log(g)
+ let person = {
+    Name: "",
+    FatherName:"",
+    Age:"",
+    Num:""
+
+   }
+
+   let person1 = { ...person };
+   let person2 = { ...person };
+   let person3 = { ...person };
+   let person4 = { ...person };
+   
+   const PERSONS = [person, person1, person2, person3, person4];
+   
+   PERSONS[0].Age=23
+   PERSONS[0].FatherName="Syed"
+   PERSONS[0].Num=1212
+   PERSONS[0].Name="Syed Kazim"
+   
+   PERSONS[1].Age=12
+   PERSONS[1].FatherName="saeeed"
+   PERSONS[1].Num=2334
+   PERSONS[1].Name="apocho"
+
+   PERSONS[2].Age=34
+   PERSONS[2].FatherName="agha"
+   PERSONS[2].Num=1288
+   PERSONS[2].Name="syyyed"
+
+   PERSONS[3].Age=50
+   PERSONS[3].FatherName="aghajan"
+   PERSONS[3].Num=66666
+   PERSONS[3].Name="aghaaaaaaaa"
+
+
+
+  
+    console.log(PERSONS)
+    
+ 
+    
+   
+
+
+
+
+
