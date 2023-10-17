cf6d849ac1d5cb3fc5e671876a478da17233938c
saqlain shah <cyedcaqlain1@gmail.com>
2023-10-17 16:38:18 +0500

    comparsion and logical operater

---
+++ frontend/JS/Arithmetic.js
@@ -0,0 +1,31 @@
+let number1 = 10;
+let number2 = 50;
+
+//Code Example of Addtion
+let addition = number1 + number2;
+console.log("Addition :", addition);
+
+//Code Example of Subtraction
+let Subtraction = number1 - number2;
+console.log("Subtraction :", Subtraction);
+
+//Code Example of Multiplication
+let Multiplication = 3 * 2;
+console.log("Multiplication :", Multiplication);
+
+//Code Example of Exponentiation
+let Exponentiation = 3 ** 3;
+console.log("Exponentiation :", Exponentiation);
+
+//Code Example of Division
+let Division = number1 / number2;
+console.log("Division :", Division);
+
+//Code Example of Modulus
+let Modulus = number1 % number2;
+console.log("Modulus :", Modulus);
+
+let Number1 = 10;
+let Number2 = 10;
+console.log("Applied Post Decrement Operator: ", Number1 * Number2--);
+console.log("Applied  Pre Increment Operator: ", ++Number1 * Number2);
---
+++ frontend/JS/demo.js
@@ -0,0 +1,12 @@
+const Person = {
+  name: {
+    firstName: "Syed",
+    SurName: "Saqlain",
+    lastName: "Abbas",
+  },
+  number: [4803, 7572, 6102],
+  gender: "male",
+};
+const Persons = [{}];
+
+console.log("Persons Details: ", Persons);
