c3f22c0926d72a10d2b20bfe6792fd3115b4592f
saqlain shah <cyedcaqlain1@gmail.com>
2023-10-18 16:09:02 +0500

    added function

--- frontend/JS/Arithmetic.js
+++ frontend/JS/Arithmetic.js
@@ -1,30 +1,52 @@
-let number1 = 10;
-let number2 = 50;
+let number1 = 5;
+let number2 = 2;
 
 //Code Example of Addtion
-let addition = number1 + number2;
-console.log("Addition :", addition);
+
+let Addition = (number1, number2) => {
+  let sum = number1 + number2;
+  console.log("Addition :", sum);
+};
 
 //Code Example of Subtraction
-let Subtraction = number1 - number2;
-console.log("Subtraction :", Subtraction);
+let Subtraction = (number1, number2) => {
+  let sub = number1 - number2;
+  console.log("Subtraction :", sub);
+};
 
-//Code Example of Multiplication
-let Multiplication = 3 * 2;
-console.log("Multiplication :", Multiplication);
+function Multiplication(number1, number2) {
+  let Mul = number1 * number2;
+  console.log("Multiplication :", Mul);
+}
 
 //Code Example of Exponentiation
-let Exponentiation = 3 ** 3;
-console.log("Exponentiation :", Exponentiation);
-
+function Exponentiation(number1, number2) {
+  let Exp = number1 ** number2;
+  console.log("Exponentiation :", Exp);
+}
 //Code Example of Division
-let Division = number1 / number2;
-console.log("Division :", Division);
+let Division = (number1, number2) => {
+  let Div = number1 / number2;
+  console.log("Division :", Div);
+};
 
 //Code Example of Modulus
-let Modulus = number1 % number2;
-console.log("Modulus :", Modulus);
+let Modulus = (number1, number2) => {
+  let Mod = number1 % number2;
+  console.log("Modulus :", Mod);
+};
 
+Addition(number1, number2);
+Addition(78, 22);
+Addition(22, 22);
+Addition(32, 41);
+Addition(21, 44);
+Addition(11, 33);
+Multiplication(number1, number2);
+Modulus(number1, number2);
+Subtraction(number1, number2);
+Division(number1, number2);
+Exponentiation(number1, number2);
 let Number1 = 10;
 let Number2 = 10;
 console.log("Applied Post Decrement Operator: ", Number1 * Number2--);
