522c61e0ba4b6c3817c134e92c7684b45bec7b2c
bruanees <bruanees@gmail.com>
2023-11-01 19:26:55 +0500

    test.js added

---
+++ frontend/JS/test.js
@@ -0,0 +1,19 @@
+
+//alert ('Hellow, World ');
+console.log('Hello World')
+console.log("Hellow, World")
+// using arrays square brackets
+let fruits= ['apple', ' graph','banana', 7];
+console.log (fruits[3]);
+// using the Array constructor
+let colors= ('red','green','blue');
+console.log (colors[2]);
+console.log (fruits);
+console.log (colors);
+
+console.log('**********************');
+ a = 10;
+ b = 25;
+ sum= a+b;
+console.log(sum);
+scanner sc = new
