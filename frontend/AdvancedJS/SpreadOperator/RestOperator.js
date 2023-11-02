1ddc724d7f1ee1d03af9674bbac32bb4ae3285f1
bruanees <bruanees@gmail.com>
2023-11-02 20:15:07 +0500

    hi

--- frontend/AdvancedJS/SpreadOperator/RestOperator.js
+++ frontend/AdvancedJS/SpreadOperator/RestOperator.js
@@ -10,3 +10,11 @@ function average(...args) {
 }
 
 average(1, 2, 3, 4, 5);
+
+[1, 2, 3, 4, 5, 6];
+let a = [1, 2, 3];
+let b = [4, 5, 6];
+//const c = a.concat(b);
+const d = [...a, ...b];
+console.log(d);
+//console.log(d);
