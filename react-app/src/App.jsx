78ae8ec5934638ba4891e7d974d47702e9b523dd
saqlain shah <cyedcaqlain1@gmail.com>
2023-11-21 16:12:04 +0500

    new componenet for state and props

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,11 +1,17 @@
 import FirstComp from "./components/FirstComp"
 import SecondComp from "./components/SecondComp"
+//import ChildComponent from "./components/child"
+import Counter from "./components/counter"
+import Parent from "./components/parent"
+
 function App() {
   return (
     <>
       <h1>My First React App</h1>
-      <FirstComp/>
-      <SecondComp/>
+      <FirstComp />
+      <SecondComp />
+      <Counter />
+      <Parent />
     </>
   )
 }
